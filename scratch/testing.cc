de0a89c2e26a15e6d5e05e26317a1d2f2ae629a6
yunminjin2 <yunminjin2@gmail.com>
2022-05-31 17:19:13 +0000

    update testing topology

--- scratch/testing.cc
+++ scratch/testing.cc
@@ -13,37 +13,23 @@ using namespace std;
 
 NS_LOG_COMPONENT_DEFINE("TeamP");
 
+uint32_t accumPackets = 0;
 static void
-Rxtime(std::string context, Ptr<const Packet> p, const Address &a) {
-    static double bytes1, bytes2, b1_time, b2_time = 0;
-    double time = Simulator::Now().GetSeconds();
-
-    if(context == "Flow1") {
-        bytes1 += p->GetSize();
-        if (time - b1_time >= 0.1) {
-            NS_LOG_UNCOND("1\t" << Simulator::Now().GetSeconds()  << "\t" << bytes1*8/1000000/(Simulator::Now().GetSeconds()-1));
-            b1_time = time;
-        }
-    }
-
-    else if(context == "Flow2") {
-        bytes2 += p->GetSize();
-        if (time - b2_time >= 0.1) {
-            NS_LOG_UNCOND("2\t" << Simulator::Now().GetSeconds()  << "\t" << bytes1*8/1000000/(Simulator::Now().GetSeconds()-1));
-            b2_time = time;
-        }
-    }
+PacketCount(Ptr<const Packet> p)
+{
+    accumPackets += p->GetSize();
+	NS_LOG_LOGIC(accumPackets);
 }
 
 int  main(int argc, char*argv[])
 {
     LogComponentEnable("TeamP", LOG_LEVEL_ALL);
-
-    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable("BufBridgeNetDevice", LOG_LEVEL_ALL);
-    LogComponentEnable("BridgeNetDevice", LOG_LEVEL_ALL);
-	LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-    //LogComponentEnable("OnOffApplication", LOG_LEVEL_ALL);
+    // LogComponentEnable("CsmaNetDevice", LOG_LEVEL_ALL);
+    // LogComponentEnable("BufBridgeNetDevice", LOG_LEVEL_DEBUG);
+    // LogComponentEnable("BridgeNetDevice", LOG_LEVEL_ALL);
+    // LogComponentEnable("PacketSink", LOG_LEVEL_ALL);
+	// LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    // LogComponentEnable("OnOffApplication", LOG_LEVEL_ALL);
     NodeContainer terminals;
     terminals.Create(3);
 
@@ -51,7 +37,7 @@ int  main(int argc, char*argv[])
     bridgeSwitch.Create(1);
 
 	CsmaHelper csma;
-	csma.SetChannelAttribute("DataRate", StringValue("5Mbps"));
+	csma.SetChannelAttribute("DataRate", StringValue("2.5Mbps"));
 	csma.SetChannelAttribute("Delay", StringValue("10us"));
 
 	NetDeviceContainer csmaDevices;
@@ -64,7 +50,7 @@ int  main(int argc, char*argv[])
     }
     Ptr<Node> switchNode = bridgeSwitch.Get(0);
     BufBridgeHelper bridge; 
-
+    // bridge.SetDeviceAttribute("EnableLearning", BooleanValue(false));
     bridge.Install(switchNode, switchDevices);
 
 	InternetStackHelper stack;
@@ -76,49 +62,38 @@ int  main(int argc, char*argv[])
 
     uint16_t port = 9;
 
+    // Application executing
+    OnOffHelper onoff1("ns3::UdpSocketFactory", Address(InetSocketAddress(interfaces.GetAddress(0), port)));
+    onoff1.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=5]"));
+    onoff1.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+    onoff1.SetAttribute("DataRate", DataRateValue(250000));
 
-    ///////////////////////////// Flow1  ///////////////////////////////////  UDP or TCP
-	UdpEchoClientHelper echoClient(interfaces.GetAddress(2), port);
-	echoClient.SetAttribute("MaxPackets", UintegerValue(1500));
-	echoClient.SetAttribute("Interval", TimeValue(Seconds(0.5)));
-	echoClient.SetAttribute("PacketSize", UintegerValue(1050));
-    NS_LOG_INFO("echoClient Setted");
+    ApplicationContainer onoffApp1 = onoff1.Install(terminals.Get(2));
+    onoffApp1.Start(Seconds(1.0));
+    onoffApp1.Stop(Seconds(16.0));
 
-    ApplicationContainer echoClientpp1 = echoClient.Install(terminals.Get(0));
-    echoClientpp1.Start(Seconds(0.5));
-    echoClientpp1.Stop(Seconds(16.0));
-
-    ApplicationContainer echoClientpp2 = echoClient.Install(terminals.Get(1));
-    echoClientpp2.Start(Seconds(1.3));
-    echoClientpp2.Stop(Seconds(16.0));
-
-	// UdpEchoServerHelper echoServer(9);
-	// ApplicationContainer serverApps(echoServer.Install(terminals.Get(2)));
-	// serverApps.Start(Seconds(0));
-	// serverApps.Stop(Seconds(16.0));
-
-    OnOffHelper onoff2("ns3::UdpSocketFactory", Address(InetSocketAddress(Ipv4Address("10.1.1.1"), port)));
-    onoff2.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=0.5]"));
-    onoff2.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0.5]"));
+    OnOffHelper onoff2("ns3::UdpSocketFactory", Address(InetSocketAddress(interfaces.GetAddress(1), port)));
+    onoff2.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=5]"));
+    onoff2.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
     onoff2.SetAttribute("DataRate", DataRateValue(250000));
 
     ApplicationContainer onoffApp2 = onoff2.Install(terminals.Get(2));
-    onoffApp2.Start(Seconds(1.0));
+    onoffApp2.Start(Seconds(1.2));
     onoffApp2.Stop(Seconds(16.0));
 
-    // PacketSinkHelper sink1("ns3::UdpSocketFactory", Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
-    // ApplicationContainer sinkApp1 = sink1.Install(terminals.Get(0));
-    // sinkApp1.Start(Seconds(1.0)); 
-    // sinkApp1.Get(0)->TraceConnect("Rx", "Flow1", MakeCallback(&Rxtime));  
-    
+    PacketSinkHelper sink1("ns3::UdpSocketFactory", Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
+    ApplicationContainer sinkApp1 = sink1.Install(terminals.Get(0));
+    sinkApp1.Start(Seconds(1.0)); 
+
     PacketSinkHelper sink2("ns3::UdpSocketFactory", Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
-    ApplicationContainer sinkApp2 = sink2.Install(terminals.Get(0));
+    ApplicationContainer sinkApp2 = sink2.Install(terminals.Get(1));
     sinkApp2.Start(Seconds(1.0)); 
-    sinkApp2.Get(0)->TraceConnect("Rx", "Flow2", MakeCallback(&Rxtime));
     ////////////////////////////////////////////////////////////////////////
 
+    csmaDevices.Get(2)->TraceConnectWithoutContext("MacTx", MakeCallback(&PacketCount));
 
-    Simulator::Stop(Seconds(5));
+
+    Simulator::Stop(Seconds(2));
 	Simulator::Run();
 	Simulator::Destroy();
 
